2985548a914ad0d6aeded7cf94f124a9094a6d1b
flpdorea <felipeldorea@gmail.com>
2022-09-18 18:18:00 -0300

    First commit

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express"
+import CreateCourseService from "./CreateCourseService"
+
+export function createCourse(req: Request, res: Response) {
+    CreateCourseService.execute({ name: 'NodeJS', educator: 'Dani' })
+
+    return res.send()
+}
